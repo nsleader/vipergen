4dfe35a18fe4fe7e220da440136681402535707f
Ivan Ruman <telcompu@gmail.com>
2015-12-30 13:47:58 +0400

    Initial Commit

---
+++ vipergen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vipergen
+//
+//  Created by IVAN CHIRKOV on 30.12.15.
+//  Copyright © 2015 IVAN CHIRKOV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
